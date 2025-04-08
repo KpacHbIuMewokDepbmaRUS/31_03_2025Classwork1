e43624e02a534a76891dd5ae905857ac14bad796
Я умераю < @DESKTOP-G6D5GU2>
2025-04-09 01:13:40 +0400

    Добавьте файлы проекта.

---
+++ 31_03_2025КласснаяРабота/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_03_2025КласснаяРабота
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание4");
+            Console.Title = "Mr. PEPSi";
+            Console.WriteLine("Жмай что нибудь");
+            Console.ReadKey();
+            Console.WriteLine("“C# — это современный, \n\tс открытым исходным кодом, \n\t\tобъектно-ориентированный \n\t\t\tязык программирования.\r\n");
+            Console.Write("Задание5");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("“C# — это современный,");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\tс открытым исходным кодом,");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("\n\t\tобъектно-ориентированный");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("\n\t\t\tязык программирования.");
+            Console.ReadKey();
+
+        }
+    }
+}
